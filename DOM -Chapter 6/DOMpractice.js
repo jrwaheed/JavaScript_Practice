5c493c42b74a4e50b6dcfe0d79e2e2b00b5c9c3b
jrwaheed <j.r.waheed@gmail.com>
2021-03-20 06:12:01 +0100

    JavaScript work

---
+++ DOM -Chapter 6/DOMpractice.js
@@ -0,0 +1,11 @@
+
+
+function init() {
+    var gPlanet = document.getElementById("greenplanet");
+    gPlanet.innerHTML = "Red Alert: hit by phaser fire!";
+    gPlanet.setAttribute("class","redtext")
+}
+
+window.onload = init;
+
+
